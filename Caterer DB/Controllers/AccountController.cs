a8cb7d8b2c454980ea1d086e0909d15bfe982f42
Sebastian Bünck <buenck@gmx.de>
2017-01-23 19:38:37 +0100

    Login Seite korrigiert, Fehlermeldungen lösen nun kein verrutschen der Inhalte aus

--- Caterer DB/Controllers/AccountController.cs
+++ Caterer DB/Controllers/AccountController.cs
@@ -87,7 +87,7 @@ namespace Caterer_DB.Controllers
                     return RedirectToAction("SendCode", new { ReturnUrl = returnUrl, RememberMe = model.RememberMe });
                 case SignInStatus.Failure:
                 default:
-                    ModelState.AddModelError("", "Invalid login attempt.");
+                    ModelState.AddModelError("", "E-Mail oder Passwort falsch.");
                     return View(model);
             }
         }
