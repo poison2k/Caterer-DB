c4baddc1193d583e3892e1839f89ec03674bbfad
Ulle <Ulle@ULLE-PC>
2017-01-26 18:26:19 +0100

    Registrierungsseite angefangen

--- Caterer DB/Models/AccountViewModels.cs
+++ Caterer DB/Models/AccountViewModels.cs
@@ -70,9 +70,9 @@ namespace Caterer_DB.Models
         public string Email { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [StringLength(100, ErrorMessage = "Das {0} muss mindestens {2} Zeichen lang sein.", MinimumLength = 6)]
         [DataType(DataType.Password)]
-        [Display(Name = "Password")]
+        [Display(Name = "Passwort")]
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
@@ -89,9 +89,9 @@ namespace Caterer_DB.Models
         public string Email { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [StringLength(100, ErrorMessage = "Das {0} muss mindestens {2} Zeichen lang sein.", MinimumLength = 6)]
         [DataType(DataType.Password)]
-        [Display(Name = "Password")]
+        [Display(Name = "Passwort")]
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
