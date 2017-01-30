190fcf0818632f6967a985092f8defe4d552d8bb
Daywalker2k3 <Daywalker2k3@DAYWALKER2K3-PC>
2017-01-30 10:20:04 +0100

    Hinzufügen von Grundstruktur für Selenium-Tests

--- SeleniumTests/Tests Edge.cs
+++ SeleniumTests/Tests Edge.cs
@@ -1,13 +1,13 @@
 using NUnit.Framework;
 using OpenQA.Selenium;
-using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Edge;
 using System;
 using System.Text;
 
 namespace SeleniumTests
 {
     [TestFixture]
-    public class TestLogin
+    public class TestLoginEdge
     {
         private IWebDriver driver;
         private StringBuilder verificationErrors;
@@ -16,7 +16,7 @@ namespace SeleniumTests
         [SetUp]
         public void SetupTest()
         {
-            driver = new FirefoxDriver();
+            driver = new EdgeDriver();
             baseURL = "http://localhost:60003/";
             verificationErrors = new StringBuilder();
         }
@@ -36,14 +36,34 @@ namespace SeleniumTests
         }
 
         [Test]
-        public void TheLoginTest()
+        public void LoginTest()
         {
-            driver.Navigate().GoToUrl(baseURL + "/Account/Login");
+            driver.Navigate().GoToUrl(baseURL + "Account/Login");
             driver.FindElement(By.Id("Email")).Clear();
             driver.FindElement(By.Id("Email")).SendKeys("caterer@test.de");
             driver.FindElement(By.Id("Password")).Clear();
             driver.FindElement(By.Id("Password")).SendKeys("Start#22");
             driver.FindElement(By.XPath("//input[@value='Anmelden']")).Click();
+
+            //Zwingt Selenium bis zu 10 Sekunden nach dem Element zu suchen!
+            driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(10));
+            //Sucht nach dem Element
+            driver.FindElement(By.Id("Willkommen"));
+            //Erwarteten Wert überprüfen
+            Assert.AreEqual(baseURL, driver.Url.ToString());
+
+            //Alternative Validierung
+            //Assert.AreEqual("Willkommen caterer@test.de", driver.FindElement(By.Id("Willkommen")).Text);
+            
+        }
+        [Test]
+        public void AGBAufruf()
+        {
+            driver.Navigate().GoToUrl(baseURL);
+            driver.FindElement(By.Id("AGB")).Click();
+            driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(10));
+
+            Assert.AreEqual( baseURL + "home/AGB", driver.Url.ToString());
         }
     }
 }
