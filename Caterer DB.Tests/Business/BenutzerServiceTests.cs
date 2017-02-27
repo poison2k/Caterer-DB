f1d38f01e2f14f07ee7b9165b03e1a9db78e4c97
milus <milus@DESKTOP-14M9CMQ>
2017-02-27 16:10:18 +0100

    BenutzerServiceTest 60%

--- Caterer DB.Tests/Business/BenutzerServiceTests.cs
+++ Caterer DB.Tests/Business/BenutzerServiceTests.cs
@@ -3,19 +3,18 @@ using Business.Interfaces;
 using Common.Interfaces;
 using DataAccess.Interfaces;
 using DataAccess.Model;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NUnit.Framework;
 using Ploeh.AutoFixture;
 using System.Linq;
 using Moq;
-using Caterer_DB.Models;
-using Caterer_DB.Models.ViewModelServices;
+using Business.Services;
+using System.Collections.Generic;
 
 namespace BusinessBenutzerServiceTest
 {
 
     [TestFixture]
-    public class BenutzerGruppenViewModelTest
+    public class BusinessBenutzerServiceTest
     {
         public IBenutzerRepository MockBenutzerRepository { get; set; }
         public IBenutzerGruppeService MockBenutzerGruppeService { get; set; }
@@ -30,9 +29,13 @@ namespace BusinessBenutzerServiceTest
             Fixture = new Fixture();
             Fixture.Behaviors.OfType<ThrowingRecursionBehavior>().ToList().ForEach(b => Fixture.Behaviors.Remove(b));
             Fixture.Behaviors.Add(new OmitOnRecursionBehavior());
-        }
 
-      
+            MockMapper = new Mock<IMapper>().Object;
+            MockMD5Hash = new Mock<IMd5Hash>().Object;
+            MockMailService = new Mock<IMailService>().Object;
+
+        }
+             
 
         [Test]
         public void SearchUserByIdTest()
@@ -44,93 +47,105 @@ namespace BusinessBenutzerServiceTest
             mockBenutzerRepository.Setup(r => r.SearchUserById(It.IsAny<int>())).Returns(benutzer);
             MockBenutzerRepository = mockBenutzerRepository.Object;
 
+            var benutzerService = new BenutzerService(MockBenutzerRepository, MockMailService, MockBenutzerGruppeService, MockMD5Hash);
+
+            //Act
+            var result = benutzerService.SearchUserById(id);
+
+            //Assert
+            Assert.AreEqual(benutzer.GetType(), result.GetType());
+
         }
 
         [Test]
         public void SearchUserByIdNoTrackingTest()
         {
+            //Assert
+            var benutzer = Fixture.Build<Benutzer>().Create();
+            var id = benutzer.BenutzerId;
+            var mockBenutzerRepository = new Mock<IBenutzerRepository>();
+            mockBenutzerRepository.Setup(r => r.SearchUserByIdNoTracking(It.IsAny<int>())).Returns(benutzer);
+            MockBenutzerRepository = mockBenutzerRepository.Object;
 
+            var benutzerService = new BenutzerService(MockBenutzerRepository, MockMailService, MockBenutzerGruppeService, MockMD5Hash);
+
+            //Act
+            var result = benutzerService.SearchUserByIdNoTracking(id);
+
+            //Assert
+            Assert.AreEqual(benutzer.GetType(), result.GetType());
         }
 
         [Test]
         public void SearchUserByEmailTest()
         {
+            //Assert
+            var benutzer = Fixture.Build<Benutzer>().Create();
+            var eMail = benutzer.Mail;
+            var mockBenutzerRepository = new Mock<IBenutzerRepository>();
+            mockBenutzerRepository.Setup(r => r.SearchUserByEMail(It.IsAny<string>())).Returns(benutzer);
+            MockBenutzerRepository = mockBenutzerRepository.Object;
 
+            var benutzerService = new BenutzerService(MockBenutzerRepository, MockMailService, MockBenutzerGruppeService, MockMD5Hash);
+
+            //Act
+            var result = benutzerService.SearchUserByEmail(eMail);
+
+            //Assert
+            Assert.AreEqual(benutzer.GetType(), result.GetType());
         }
 
-        [TestMethod()]
+        [Test]
         public void FindAllBenutzersTest()
         {
+            //Assert
+           var benutzer = Fixture.Build<Benutzer>().Create();
+            var eMail = benutzer.Mail;
+            var mockBenutzerRepository = new Mock<IBenutzerRepository>();
+            mockBenutzerRepository.Setup(r => r.SearchUser());
+            MockBenutzerRepository = mockBenutzerRepository.Object;
 
+            var benutzerService = new BenutzerService(MockBenutzerRepository, MockMailService, MockBenutzerGruppeService, MockMD5Hash);
+
+            //Act
+            var result = benutzerService.FindAllBenutzers();
+
+            //Assert
+            Assert.IsNull(result);
         }
 
-        [TestMethod()]
+        [Test]
         public void FindAllMitarbeiterWithPagingTest()
         {
 
         }
 
-        [TestMethod()]
-        public void AddBenutzerTest()
-        {
 
-        }
-
-        [TestMethod()]
-        public void AddMitarbeiterTest()
-        {
-
-        }
-
-        [TestMethod()]
-        public void RegisterBenutzerTest()
-        {
-
-        }
-
-        [TestMethod()]
-        public void ForgottenPasswordEmailForBenutzerTest()
-        {
-
-        }
-
-        [TestMethod()]
+        [Test]
         public void EditBenutzerTest()
         {
 
         }
-
-        [TestMethod()]
-        public void RemoveBenutzerTest()
-        {
-
-        }
-
-        [TestMethod()]
+        
+        [Test]
         public void CheckEmailForRegistrationTest()
         {
 
         }
 
-        [TestMethod()]
+        [Test]
         public void VerifyRegistrationTest()
         {
 
         }
 
-        [TestMethod()]
+        [Test]
         public void VerifyPasswordChangeTest()
         {
 
         }
 
-        [TestMethod()]
-        public void EditBenutzerPasswordTest()
-        {
-
-        }
-
-        [TestMethod()]
+        [Test]
         public void GetMitarbeiterCountTest()
         {
 
