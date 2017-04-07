aac869c56a5c9fc2873722c5ae637353a2f99044
Sebastian Bünck <buenck@gmx.de>
2017-04-07 07:12:02 +0200

    Koordinaten Service für Tester integriert

--- Caterer DB/Controllers/GoogleController.cs
+++ Caterer DB/Controllers/GoogleController.cs
@@ -1,5 +1,6 @@
 ﻿using Business.Interfaces;
 using Caterer_DB.ViewModel;
+using GoogleMaps.LocationServices;
 using System.Web.Mvc;
 
 namespace Caterer_DB.Controllers
@@ -22,8 +23,21 @@ namespace Caterer_DB.Controllers
         [HttpPost]
         public ActionResult Index(GoogleViewModel googleModel)
         {
-            
-            
+            var Adresse = new AddressData()
+            {
+                Country = googleModel.Land,
+                Zip = googleModel.PLZ,
+                City = googleModel.Ort,
+                Address = googleModel.Street,
+                
+            };
+
+            var locationService = new GoogleLocationService();
+            var point = locationService.GetLatLongFromAddress(Adresse);
+
+            googleModel.Längengrad = point.Longitude.ToString();
+            googleModel.Breitengrad = point.Latitude.ToString();
+
             var test = BenutzerService.FindeCatererNachUmkreis(googleModel.PLZ, 100);
             
             return View(googleModel);
